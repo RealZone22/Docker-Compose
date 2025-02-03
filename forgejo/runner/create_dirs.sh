d37d4a46b1148cb9a4bb1bf7b408b33cf2aadfb9
Lenny P. <lenny@petschl.org>
2025-02-03 22:33:32 +0100

    Update runner

---
+++ forgejo/runner/create_dirs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p data
+touch data/.runner
+mkdir -p data/.cache
+
+chown -R 1001:1001 data/.runner
+chown -R 1001:1001 data/.cache
+chmod 775 data/.runner
+chmod 775 data/.cache
+chmod g+s data/.runner
+chmod g+s data/.cache
